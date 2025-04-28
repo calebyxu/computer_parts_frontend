09ae42edfe375568748d8a0d37a6fe01d5c88af3
Caleb Xu <calxu@iu.edu>
2025-04-28 18:20:21 -0400

    bug fixing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/computer_parts_web_frontend/',
+  // base: '/computer_parts_web_frontend/',
   plugins: [react()]
 })
