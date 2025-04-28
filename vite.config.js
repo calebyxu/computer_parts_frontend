321a0bd6adc7ca8cc983e42507525cc6e77f7ade
Caleb Xu <calxu@iu.edu>
2025-04-28 17:35:54 -0400

    sdf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/computer/',
+  base: '/computer_parts_web_frontend/dist/',
   plugins: [react()],
 })
